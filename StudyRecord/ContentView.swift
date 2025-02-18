653ccff074214549894a93efeb7d67386b12e8ba
plm1000Leaf <plm.lm1029@gmail.com>
2025-02-18 16:42:55 +0900

    Initial Commit

---
+++ StudyRecord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyRecord
+//
+//  Created by 千葉陽乃 on 2025/02/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyRecord/StudyRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyRecordApp.swift
+//  StudyRecord
+//
+//  Created by 千葉陽乃 on 2025/02/18.
+//
+
+import SwiftUI
+
+@main
+struct StudyRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
