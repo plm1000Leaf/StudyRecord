8b9b35dc65361fcfae5e46db6ad57da44d861243
plm1000Leaf <plm.lm1029@gmail.com>
2025-02-22 13:08:04 +0900

    ファイル整理

---
+++ StudyRecord/ Views/Review/DateReviewView.swift
@@ -0,0 +1,27 @@
+//
+//  DateReviewView.swift
+//  StudyRecord
+//
+//  Created by 千葉陽乃 on 2025/02/22.
+//
+
+import SwiftUI
+
+struct DateReviewView: View {
+    var body: some View {
+        VStack{
+            Rectangle()
+                .frame(width: 392, height: 96)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            ScrollView {
+                
+            }
+        }
+
+    }
+    
+}
+
+#Preview {
+    DateReviewView()
+}
