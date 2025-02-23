4faf27ec0b01e96f06135af3fdf704e313523f9f
plm1000Leaf <plm.lm1029@gmail.com>
2025-02-23 11:38:24 +0900

    FillTodayReview枠組位置調整

--- StudyRecord/ Views/Review/DateReviewView.swift
+++ StudyRecord/ Views/Review/DateReviewView.swift
@@ -21,6 +21,7 @@ struct DateReviewView: View {
                     }
                 }
                 TodayReview
+                FillTodayReview
             }
         }
 
@@ -108,6 +109,71 @@ extension DateReviewView {
                             .foregroundColor(.blue)
                     }
                     .padding(.top, 8)
+                    
+                }
+            }
+        }
+        .padding(.bottom, 32)
+    }
+    
+    private var FillTodayReview: some View {
+        HStack(alignment: .top, spacing: 32){
+            VStack(alignment: .trailing){
+                Text("30")
+                    .font(.system(size: 32))
+                Text("金")
+                    .font(.system(size: 16))
+            }
+            ZStack{
+                Rectangle()
+                    .frame(width: 248, height: 288)
+                VStack{
+                    HStack{
+                        Rectangle()
+                            .frame(width: 96, height: 120)
+                            .foregroundColor(.blue)
+                        VStack(spacing: 8){
+                        Text("応用情報技術者合格教本")
+                            .font(.system(size: 16))
+                            .frame(width: 104)
+                            .foregroundColor(.blue)
+                        
+                            HStack{
+                                Text("30")
+                                    .font(.system(size: 16))
+                                Text("ページ")
+                                    .font(.system(size: 8))
+                            }
+                            Text("〜")
+                                .font(.system(size: 16))
+                                .bold()
+                                .rotationEffect(.degrees(90))
+                            HStack{
+                                Text("30")
+                                    .font(.system(size: 16))
+                                Text("ページ")
+                                    .font(.system(size: 8))
+                            }
+
+                        }
+                        .foregroundColor(.blue)
+                    }
+                    .padding(.bottom, 8)
+                    Rectangle()
+                        .frame(width: 208, height: 64)
+                        .foregroundColor(.blue)
+                    HStack{
+                        
+                        BasicButton(label: "確定", action: {
+                            print("確定ボタンが押されました")
+                        })
+
+                            .frame(width: 56, height: 32)
+                            .foregroundColor(.blue)
+                    }
+                    .padding(.leading, 144)
+                    .padding(.top, 8)
+
                     
                 }
             }
