be7486214f6ef4b2355174dc212b251523eb62e6
plm1000Leaf <plm.lm1029@gmail.com>
2025-02-25 14:48:45 +0900

    MonthReviewView枠組

---
+++ StudyRecord/ Views/Review/MonthReviewView.swift
@@ -0,0 +1,32 @@
+//
+//  MonthReviewView.swift
+//  StudyRecord
+//
+//  Created by 千葉陽乃 on 2025/02/25.
+//
+
+import SwiftUI
+
+struct MonthReviewView: View {
+    var body: some View {
+        VStack(alignment: .leading ,spacing: 56){
+            HStack(alignment: .bottom){
+                Text("2025")
+                    .font(.system(size: 16))
+                    .alignmentGuide(.bottom) { d in d[.firstTextBaseline] }
+                
+                Text("1")
+                    .font(.system(size: 48))
+                    .alignmentGuide(.bottom) { d in d[.firstTextBaseline] }
+            }
+            
+            Rectangle()
+                .frame(width: 336, height: 352)
+            
+        }
+    }
+}
+
+#Preview {
+    MonthReviewView()
+}
