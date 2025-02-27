65ab4d2b04b651cebfaeec99d44b885e79d11d24
plm1000Leaf <plm.lm1029@gmail.com>
2025-02-27 15:24:52 +0900

    下タブで画面切り替え

---
+++ StudyRecord/Components/MainTabView.swift
@@ -0,0 +1,61 @@
+//
+//  TabView.swift
+//  StudyRecord
+//
+//  Created by 千葉陽乃 on 2025/02/27.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+//    var selectedTab: Int
+    @State private var selectedTab = 1
+    var body: some View {
+        TabView(selection: $selectedTab){
+            DateReviewView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Review")
+                }
+                .tag(0)
+            
+            BeforeCheckView()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Check")
+                }
+                .tag(1)
+            
+            StudyPlanView()
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("Study Plan")
+                }
+                .tag(2)
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("ホーム画面")
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        Text("プロフィール画面")
+    }
+}
+
+struct SettingsView: View {
+    var body: some View {
+        Text("設定画面")
+    }
+}
+
+
+
+#Preview {
+    MainTabView()
+}
