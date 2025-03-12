2e02e3b18914978115e044be70a3162691d01990
plm1000Leaf <plm.lm1029@gmail.com>
2025-03-12 16:18:01 +0900

    feat:Reviewの画面遷移をTabViewに対応

--- StudyRecord/ Views/MainTabView.swift
+++ StudyRecord/ Views/MainTabView.swift
@@ -14,7 +14,7 @@ struct MainTabView: View {
     
     var body: some View {
         TabView(selection: $selectedTab){
-            DateReviewView(showDateReviewView: .constant(false))
+            YearReviewView()
                 .tabItem {
                     Image(systemName: "house.fill")
                     Text("Review")
