cf981b7eddc618d413fe82e1342ea031598b79ed
plm1000Leaf <plm.lm1029@gmail.com>
2025-03-18 15:18:11 +0900

    fix: InputReviewFieldのリファクタリング

--- StudyRecord/Components/InputReviewField.swift
+++ StudyRecord/Components/InputReviewField.swift
@@ -8,60 +8,78 @@ struct InputReviewField: View {
     var body: some View {
         VStack(spacing: 16) {
             if isEditing {
-                ZStack{
-                    Rectangle()
-                        .frame(width: 208, height: 64)
-                        .foregroundColor(.green)
-                    CustomTextEditor(text: $text, maxCharacters: maxCharacters)
-                        .frame(width: 200, height: 64)
-                        .padding(.leading, 8)
-                        .background(Color.white)
-                        .cornerRadius(8)
-                    
-                }
+                editReviewField
             } else {
-                ZStack{
-                    Rectangle()
-                        .frame(width: 208, height: 64)
-                        .foregroundColor(.green)
-                    Text(text.isEmpty ? "a" : text)
-                        .frame(width: 200, height: 64)
-                        .multilineTextAlignment(.leading)
-                        .lineLimit(nil) // 改行を有効化
-                        .padding(.leading, 8)
-
-                }
+                displayReviewField
             }
 
             if isEditing {
-                HStack {
-                    BasicButton(label: "確定", action: {
-                        isEditing.toggle()
-                    })
-                    .frame(width: 56, height: 40)
-                    .foregroundColor(.blue)
-                }
-                .padding(.leading, 144)
+                editReviewButton
 
             } else {
-                HStack(spacing: 120) {
-                    Circle()
-                        .frame(width: 32, height: 32)
-                        .foregroundColor(.blue)
-                    Button(action: {
-                        isEditing.toggle()
-                    }) {
-                        Circle()
-                            .frame(width: 32, height: 32)
-                            .foregroundColor(.blue)
-                    }
-                }
+                displayReviewButton
             }
         }
         .padding()
     }
 }
 
+
+extension InputReviewField {
+    private var displayReviewField: some View {
+        ZStack{
+            Rectangle()
+                .frame(width: 208, height: 64)
+                .foregroundColor(.green)
+            Text(text.isEmpty ? "a" : text)
+                .frame(width: 200, height: 64)
+                .multilineTextAlignment(.leading)
+                .lineLimit(nil) // 改行を有効化
+                .padding(.leading, 8)
+
+        }
+    }
+    
+    private var editReviewField: some View {
+        ZStack{
+            Rectangle()
+                .frame(width: 208, height: 64)
+                .foregroundColor(.green)
+            CustomTextEditor(text: $text, maxCharacters: maxCharacters)
+                .frame(width: 200, height: 64)
+                .padding(.leading, 8)
+                .background(Color.white)
+                .cornerRadius(8)
+            
+        }
+    }
+    
+    private var displayReviewButton: some View {
+        HStack(spacing: 120) {
+            Circle()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.blue)
+            Button(action: {
+                isEditing.toggle()
+            }) {
+                Circle()
+                    .frame(width: 32, height: 32)
+                    .foregroundColor(.blue)
+            }
+        }
+    }
+    
+    private var editReviewButton: some View {
+        HStack {
+            BasicButton(label: "確定", action: {
+                isEditing.toggle()
+            })
+            .frame(width: 56, height: 40)
+            .foregroundColor(.blue)
+        }
+        .padding(.leading, 144)
+    }
+}
 struct InputReviewField_Previews: PreviewProvider {
     static var previews: some View {
         InputReviewField()
