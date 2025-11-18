3a044b5ad01108e79bf362a80fc85029758642d9
plm1000Leaf <plm.lm1029@gmail.com>
2025-11-18 19:13:07 +0900

    fix: editReviewFieldをタップで編集可能に

--- Components/ReviewComponents/InputReviewField.swift
+++ Components/ReviewComponents/InputReviewField.swift
@@ -14,15 +14,10 @@ struct InputReviewField: View {
     var body: some View {
         VStack(spacing: 16) {
             if isEditing {
-                
                 editReviewField
-                editReviewButton
-                
             } else {
                 displayReviewField
-                displayReviewButton
             }
-
 
         }
         .padding()
@@ -36,55 +31,63 @@ struct InputReviewField: View {
 
 extension InputReviewField {
     private var displayReviewField: some View {
-        ZStack{
-            Rectangle()
-                .frame(width: 208, height: 64)
-                .foregroundColor(isChecked ? .mainColor10 : .notCheckedColor10)
-            Text(reviewText.isEmpty ? "振り返りを入力" : reviewText)
-                .frame(width: 200, height: 64)
-                .multilineTextAlignment(.leading)
-                .lineLimit(nil) // 改行を有効化
-                .padding(.leading, 8)
-
+        Button(action: {
+            isEditing.toggle()
+        }) {
+            ZStack{
+                Rectangle()
+                    .frame(width: 208, height: 64)
+                    .foregroundColor(isChecked ? .mainColor10 : .notCheckedColor10)
+                Text(reviewText.isEmpty ? "振り返りを入力" : reviewText)
+                    .frame(width: 200, height: 64)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(nil) // 改行を有効化
+                    .padding(.leading, 8)
+                
+            }
         }
     }
     
     private var editReviewField: some View {
-
-        CustomTextEditor(text: $reviewText, maxCharacters: maxCharacters)
+ 
+            CustomTextEditor(text: $reviewText, maxCharacters: maxCharacters)
                 .frame(width: 200, height: 64)
                 .padding(.leading, 8)
                 .background(.baseColor10)
                 .cornerRadius(8)
+//                .onSubmit {
+//                    DailyRecordManager.shared.updateReview(reviewText, for: dailyRecord, context: viewContext)
+//                    isEditing = false
+//                }
+        
     }
     
-    private var displayReviewButton: some View {
-
-            Button(action: {
-                isEditing.toggle()
-            }) {
-                ZStack{
-                    Circle()
-                        .frame(width: 32, height: 32)
-                        .foregroundColor(.mainColor0)
-                    Image(systemName:"square.and.pencil")
-                        .frame(width: 32, height: 32)
-                        .foregroundColor(.baseColor10)
-                }
-                .frame(maxWidth: .infinity, alignment:
-                        .trailing)
-                .padding(.trailing, 8)
-            }
-    }
-    
-    private var editReviewButton: some View {
-        HStack {
-            BasicButton(label: "確定", width: 56, height: 32) {
-                DailyRecordManager.shared.updateReview(reviewText, for: dailyRecord, context: viewContext)
-                isEditing = false
-            }
-
-        }
-        .padding(.leading, 144)
-    }
+//    private var displayReviewButton: some View {
+//
+//            Button(action: {
+//                isEditing.toggle()
+//            }) {
+//                ZStack{
+//                    Circle()
+//                        .frame(width: 32, height: 32)
+//                        .foregroundColor(.mainColor0)
+//                    Image(systemName:"square.and.pencil")
+//                        .frame(width: 32, height: 32)
+//                        .foregroundColor(.baseColor10)
+//                }
+//                .frame(maxWidth: .infinity, alignment:
+//                        .trailing)
+//                .padding(.trailing, 8)
+//            }
+//    }
+//    private var editReviewButton: some View {
+//        HStack {
+//            BasicButton(label: "確定", width: 56, height: 32) {
+//                DailyRecordManager.shared.updateReview(reviewText, for: dailyRecord, context: viewContext)
+//                isEditing = false
+//            }
+//
+//        }
+//        .padding(.leading, 144)
+//    }
 }
